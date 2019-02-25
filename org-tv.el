f6214144eb590972d555cefd12e74ffd3da95342
Dante Catalfamo <dante.catalfamo@shopify.com>
2019-02-25 11:29:26 -0500

    Add ability to turn show into TODO, fix packaging

--- org-tv.el
+++ org-tv.el
@@ -4,30 +4,49 @@
 ;;; subheading, and each episode as a checkbox
 
 ;;; Code:
-(defun org-tv-new (name seasons)
+(require 'org)
+
+(defun org-tv-new (name seasons todo)
   "Create Org-mode list for TV show.
-NAME is show, SEASONS is number of seasons."
-  (interactive "sShow Name: \nnNumber of seasons: ")
+NAME is show, SEASONS is number of seasons, TODO is weather or not to
+make it into a todo entry with a percentage for each season"
+  (interactive (list (read-string "Show Name: ")
+                     (read-number "Number of seasons: ")
+                     (y-or-n-p "TODO entry? ")))
 
   (unless (eq major-mode 'org-mode)
     (error "Not in org-mode"))
 
   (if (eq seasons 1)
-      (my-tv-new-season name t)
+      (org-tv-new-season name t todo)
 
-    (org-insert-subheading t)
+    (if todo
+        (org-insert-todo-subheading t)
+      (org-insert-subheading t))
     (insert name)
+    (when todo
+      (insert " [/]")
+      (org-update-statistics-cookies nil))
     (dotimes (i seasons)
-      (my-tv-new-season (concat "Season " (number-to-string (+ 1 i)))
-                        (eq i 0)))))
+      (org-tv-new-season (concat "Season " (number-to-string (+ 1 i)))
+                        (eq i 0)
+                        todo))))
 
-(defun org-tv-new-season (name &optional indent)
+(defun org-tv-new-season (name &optional indent todo)
   "Insert season, part of tv thing.
-NAME is name of the season, INDENT is weather or not to indent further."
+NAME is name of the season, INDENT is weather or not to indent
+further, TODO adds a percentage to the end of the season line"
   (if indent
-      (org-insert-subheading t)
-    (org-insert-heading t))
+      (if todo
+          (org-insert-todo-subheading t)
+        (org-insert-subheading t))
+    (if todo
+        (org-insert-todo-heading-respect-content t)
+      (org-insert-heading t)))
   (insert name)
+  (when todo
+    (insert " [%]")
+    (org-update-statistics-cookies nil))
   (let ((episodes (read-number (concat "Episodes in " name ": "))))
     (org-return-indent)
     (insert "- [ ] Ep. 1")
@@ -35,5 +54,5 @@ NAME is name of the season, INDENT is weather or not to indent further."
       (org-insert-todo-heading t)
       (insert (concat "Ep. " (number-to-string (+ 2 e)))))))
 
-(provide 'org-tv-new)
+(provide 'org-tv)
 ;;; org-tv.el ends here
