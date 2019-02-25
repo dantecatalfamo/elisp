3ead3b35f1e1af84d760a0e442e3658f6b8eee87
Dante Catalfamo <dante.catalfamo@shopify.com>
2019-02-25 10:25:42 -0500

    Init commit

---
+++ org-tv.el
@@ -0,0 +1,39 @@
+;;; Org-TV --- Easily add TV shows to Org-Mode file
+;;; Commentary:
+;;; Adds a TV show as an org heading, with each season as a
+;;; subheading, and each episode as a checkbox
+
+;;; Code:
+(defun org-tv-new (name seasons)
+  "Create Org-mode list for TV show.
+NAME is show, SEASONS is number of seasons."
+  (interactive "sShow Name: \nnNumber of seasons: ")
+
+  (unless (eq major-mode 'org-mode)
+    (error "Not in org-mode"))
+
+  (if (eq seasons 1)
+      (my-tv-new-season name)
+
+    (org-insert-subheading t)
+    (insert name)
+    (dotimes (i seasons)
+      (my-tv-new-season (concat "Season " (number-to-string (+ 1 i)))
+                        (eq i 0)))))
+
+(defun org-tv-new-season (name &optional indent)
+  "Insert season, part of tv thing.
+NAME is name of the season, INDENT is weather or not to indent further."
+  (if indent
+      (org-insert-subheading t)
+    (org-insert-heading t))
+  (insert name)
+  (let ((episodes (read-number (concat "Episodes in " name ": "))))
+    (org-return-indent)
+    (insert "- [ ] Ep. 1")
+    (dotimes (e (- episodes 1))
+      (org-insert-todo-heading t)
+      (insert (concat "Ep. " (number-to-string (+ 2 e)))))))
+
+(provide 'org-tv-new)
+;;; org-tv.el ends here
