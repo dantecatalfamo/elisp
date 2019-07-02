f1aeadade37f1b4ebc4e2cb8772df05bcb02b877
Dante Catalfamo <dante.catalfamo@shopify.com>
2019-07-02 16:14:21 -0400

    Add opening-only tags

--- html.el
+++ html.el
@@ -2,17 +2,10 @@
   "Define an HTML tag with name NAME."
   (let* ((tagname (symbol-name name))
          (tagtext (format "<%s%%s>%%s</%s>" tagname tagname)))
-    `(defmacro ,name (tags &rest inner)
+    `(defmacro ,name (properties &rest inner)
        `(format ,,tagtext
-                (attrs ',tags)
+                (attrs ',properties)
                 (apply #'concat (mapcar #'eval ',inner))))))
-
-(defmacro deftag-short (name)
-  "Define an html tag with name of NAME1 for NAME that takes no properties."
-  (let* ((tagname (symbol-name name))
-         (shorttag (intern (concat tagname "1"))))
-    `(defmacro ,shorttag (&rest inner)
-       `(,',name nil ,@inner))))
 
 (defmacro deftags (&rest tags)
   "Define nultiple TAGS in one statement."
@@ -20,6 +13,27 @@
     (dolist (tag tags defined)
       (push `(deftag ,tag) defined))
     `(progn ,@defined)))
+
+(defmacro deftag-single (name)
+  "Define an HTML tag with name NAME with no content or closing tag."
+  (let* ((tagname (symbol-name name))
+         (tagtext (format "<%s%%s>" tagname)))
+    `(defmacro ,name (&rest properties)
+       `(format ,,tagtext
+                (attrs ',properties)))))
+
+(defmacro deftags-single (&rest tags)
+  (let (defined)
+    (dolist (tag tags defined)
+      (push `(deftag-single ,tag) defined))
+    `(progn ,@defined)))
+
+(defmacro deftag-short (name)
+  "Define an html tag with name of NAME1 for NAME that takes no properties."
+  (let* ((tagname (symbol-name name))
+         (shorttag (intern (concat tagname "1"))))
+    `(defmacro ,shorttag (&rest inner)
+       `(,',name nil ,@inner))))
 
 (defmacro deftags-short (&rest tags)
   "Define multiple short TAGS in one statement."
@@ -43,7 +57,8 @@ with the form (PROPERTY VALUE1 VALUE2 VALUE2)."
    (properties (format " %s=\"%s\"" (car properties) (mapconcat #'identity (cdr properties) " ")))
    (t nil)))
 
-(deftags1 html head title body div p b script style link span)
+(deftags1 html head title body div p b script style span)
+(deftags-single br link)
 
 (html1
  (head1 (title1 "This is the page, baby"))
@@ -53,5 +68,6 @@ with the form (PROPERTY VALUE1 VALUE2 VALUE2)."
         "here we are, this is the sauce heaven")
    (div (id "thing")
         (span1 "Big"))
+   (br)
    (div (class "footer")
         "This is the bottom"))))
