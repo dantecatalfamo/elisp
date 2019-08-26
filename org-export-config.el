242ed05ef35759a43ac65760cd63990685a5cb76
Dante Catalfamo <dante.catalfamo@shopify.com>
2019-08-26 17:47:00 -0400

    Remove example

--- org-export-config.el
+++ org-export-config.el
@@ -19,13 +19,7 @@ Example:
     :SSH_USER:    john
     :SSH_PORT:    2222
     :SSH_FORWARD: t
-    :END:
-becomes:
-Host server
-  HostName 192.168.0.2
-  ForwardAgent yes
-  Port 2222
-  User john"
+    :END:"
   (with-current-buffer buffer
     (org-element-map (org-element-parse-buffer) 'headline
       (lambda (node)
